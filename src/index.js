8baef71698fc1ccb2d95e214af1bb11eda16d83a
georgeoikonomitsios <32417846+georgeoikonomitsios@users.noreply.github.com>
2022-02-03 03:24:51 +0300

    Add time and styles changes

--- src/index.js
+++ src/index.js
@@ -5,3 +5,37 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const d = new Date();
+const time = d.getHours();
+
+const customStyle = {
+  color: "red"
+};
+
+const getH1 = (time) => {
+  var greeting = "Hello";
+  if (time >= 0 || time < 12) {
+    greeting = "Good Morning";
+    customStyle.color = "red";
+  } else if (time >= 12 || time < 18) {
+    greeting = "Good Afternoon";
+    customStyle.color = "green";
+  } else {
+    greeting = "Good Evening";
+    customStyle.color = "blue";
+  }
+  return greeting;
+};
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" style={customStyle}>
+      {getH1(time)}
+    </h1>
+  </div>,
+  document.getElementById("root")
+);
